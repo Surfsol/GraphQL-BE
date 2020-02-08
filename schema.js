ab70b40569e18ca20bbf62b310641718d9f10db5
Surfsol <surfsol@protonamil.com>
2020-02-08 14:42:41 -0300

    add launch by flightnubmer

--- schema.js
+++ schema.js
@@ -34,16 +34,47 @@ const RocketType = new GraphQLObjectType({
 });
 
 // Root Query, similar to .get data
+//ex. search:
+// {
+//     launches{
+//       mission_name,
+//       flight_number,
+//       launch_date_local,
+//       rocket {
+//         rocket_id
+//         rocket_name
+//         rocket_type
+//       }
+//     }
+//   }
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
     //launches - want an array of launches -> use GraphQLList
     launches: {
+      //need list GraphQLList
       type: new GraphQLList(LaunchType),
       // axios because going to an api, otherwise could retrieve from a database
       resolve(parent, args) {
         return axios
           .get("https://api.spacexdata.com/v3/launches")
+          .then(res => res.data);
+      }
+    },
+    //get a single launch, .get by id
+    //ex.search : {
+    //   launch(flight_number: 2){
+    //     mission_name
+    //   }
+    // }
+    launch: {
+      type: LaunchType,
+      args: {
+        flight_number: { type: GraphQLInt }
+      },
+      resolve(parent, args) {
+        return axios
+          .get(`https://api.spacexdata.com/v3/launches/${args.flight_number}`)
           .then(res => res.data);
       }
     }
